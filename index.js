471fc555b07172ad8069bbb19945f62b6aae605b
David Rafe <david.rafe@gmail.com>
2021-05-12 19:19:42 -0400

    commit tree c962a3b13cc934a335615aecd8d880d8f3753899

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver(name) {
+    drivers.push(name)
+}
+
+function destructivelyPrependDriver(name) {
+    drivers.unshift(name)
+}
+
+function destructivelyRemoveFirstDriver() {
+    drivers.shift()
+}
+
+function destructivelyRemoveLastDriver() {
+    drivers.pop()
+}
+
+function appendDriver(name) {
+    const newDrivers = [...drivers, name]
+   return newDrivers 
+}
+
+function prependDriver(name) {
+    const newDrivers = [name, ...drivers]
+    return newDrivers
+}
+
+function removeLastDriver() {
+    const newDrivers = [...drivers]
+    newDrivers.splice(-1)
+    return newDrivers
+}
+
+function removeFirstDriver() {
+    const newDrivers = [...drivers]
+    newDrivers.splice(0, 1)
+    return newDrivers
+}
